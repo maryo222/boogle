a1b08f6330a59988939f67fcad194f4c8b6ee9d1
maryo222 <maryanneorton@gmail.com>
2017-03-02 08:41:10 +1300

    Added thanks page
    Returns from feedback page

--- thanks.php
+++ thanks.php
@@ -1,4 +1,8 @@
-<?php include 'partials/header.php'; ?>
+<?php
+include 'partials/header.php';
+$name = $_POST["name"];
+$email = $_POST["email"];
+?>
 
 <h1>
   <a href="index.php">
@@ -6,8 +10,8 @@
   </a>
 </h1>
 
-<h2 class="text-center">Thanks for the feedback <?= $name ?></h2>
-<h2 class="text-center text-muted">We will reply to <?= $email ?> if we need to follow up.</h2>
+<h2 class="text-center">Thanks for the feedback  <?= $name ?>!<br></h2>
+<h2 class="text-center text-muted">We will reply to <?= $email  ?>!<br> if we need to follow up.</h2>
 <div class="text-center">
   <a href="index.php">Home</a>
 </div>
